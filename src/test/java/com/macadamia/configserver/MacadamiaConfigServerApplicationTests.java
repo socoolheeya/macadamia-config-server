6d584220de023d087b78a4121d34ed187a05d6cd
LeeWonhee <lwh0102@gmail.com>
2022-12-30 20:55:36 +0900

    Initial commit

---
+++ src/test/java/com/macadamia/configserver/MacadamiaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macadamia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacadamiaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
